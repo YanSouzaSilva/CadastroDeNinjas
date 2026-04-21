001c86b8d72f107a26098a18a3718739032a494c
Yan Afonso <yanafonso@gmail.com>
2026-04-21 12:02:49 -0300

    feat: Inclui a rota de cadastro de ninjas usando metodo POST, tambem inicializa o Postman

--- src/main/java/dev/java10x/CadastroDeNinjas/Missoes/MissoesController.java
+++ src/main/java/dev/java10x/CadastroDeNinjas/Missoes/MissoesController.java
@@ -1,9 +1,17 @@
 package dev.java10x.CadastroDeNinjas.Missoes;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("missoes")
 public class MissoesController {
+
+    private MissoesService missoesService;
+
+    public MissoesController(MissoesService missoesService) {
+        this.missoesService = missoesService;
+    }
 
     // Adicionar missao (CREATE)
     // POST -- Mandar(postar) uma requisicao para criar as missoes
@@ -15,8 +23,15 @@ public class MissoesController {
     // Mostrar todas as missoes (READ)
     // GET -- Pegar uma requisicao para mostrar as missoes
     @GetMapping("/listar")
-    public String mostrarMissao(){
-        return "Mostrando todas missoes.";
+    public List<MissoesModel> listarMissoes(){
+        return missoesService.listarMissoes();
+    }
+
+    // Mostrar missoes por ID(READ)
+    // GET -- Pegar uma requisicao para mostrar missoes pelo ID
+    @GetMapping("/listarId/{id}")
+    public MissoesModel listarMissoesPorId(@PathVariable Long id){
+        return missoesService.listarMissoesPorId(id);
     }
 
     // Alterar dados das missoes por ID (UPDATE)
